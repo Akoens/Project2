630679774a2d010faba098f752f7b676af0973d8
Fenna <p>
2019-01-31 16:04:05 +0100

    Create MoreButtons.java

---
+++ src/Generator/MoreButtons.java
@@ -0,0 +1,10 @@
+package Parkeerbeheer.buttons;
+import Parkeerbeheer.Models;
+
+public abstract class MoreButtons {
+    protected Simulator simulator;
+
+    public MoreButtons(Simulator sim){
+    this.simulator = sim;
+    }
+}
